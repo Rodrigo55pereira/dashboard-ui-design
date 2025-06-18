9d8781123e90ad2e5b039b476806663a62acae19
Rodrigo55pereira <rodrigo55pereira.com>
2025-06-18 09:29:48 -0300

    adicao do collapsed

--- src/layouts/header.jsx
+++ src/layouts/header.jsx
@@ -1,3 +1,10 @@
-export const Header = () => {
-    return <header>Header</header>;
+import PropTypes from "prop-types";
+
+export const Header = (props) => {
+    return <header className="h-[60px] relative z-10 flex items-center justify-between bg-white px-4 shadow-md transition-colors dark:bg-slate-900">Header</header>;
+};
+
+Header.propsTypes = {
+    collapsed: PropTypes.bool,
+    setCollapsed: PropTypes.func,
 };
--- src/routes/layout.jsx
+++ src/routes/layout.jsx
@@ -9,18 +9,27 @@ import { useRef } from "react";
 export const Layout = () => {
     const isDesktopDevice = useMediaQuery("(min-width: 768px)");
 
-    const [collapsed, setCollapsed] = useState(false);
+    const [collapsed, setCollapsed] = useState(true);
 
     const sidebarRef = useRef(null);
 
     return (
-        <div className="min-h-screen bg-slate-100 transition-colors dark:bg-slate-900">
+        <div className="min-h-screen bg-slate-100 transition-colors dark:bg-slate-950">
+            <div
+                className={cn(
+                    "pointer-events-none fixed inset-0 -z-10 bg-black opacity-0 transition-opacity",
+                    !collapsed && "max-md:pointer-events-auto max-md:z-50 max-md:opacity-30",
+                )}
+            />
             <Sidebar
                 ref={sidebarRef}
                 collapsed={collapsed}
             />
-            <div className={cn("transition-[margin] duration-300")}>
-                <Header />
+            <div className={cn("transition-[margin] duration-300", collapsed ? "md:ml-[70px]" : "md:ml-[240px]")}>
+                <Header
+                    collapsed={collapsed}
+                    setCollapsed={setCollapsed}
+                />
                 <div className="h-[calc(100vh-60px)] overflow-x-hidden overflow-y-auto p-6">
                     <Outlet />
                 </div>
